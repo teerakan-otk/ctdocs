d601c25f60f955fc3ea1b57a6d17b2435f18298e
teerakan-otk <teerakan.otk@outlook.com>
2026-01-11 23:18:48 +0700

    feat: enable dynamic YouTube video embedding by accepting video ID as a prop

--- components/embed-youtube.jsx
+++ components/embed-youtube.jsx
@@ -1,10 +1,10 @@
 "use client";
 
-export function EmbedYoutube() {
+export function EmbedYoutube({ id }) {
   return (
     <iframe
       className="aspect-video w-full"
-      src="https://www.youtube.com/embed/HYjlkiWpeKU?si=Rhx4wZkX0FSsXOC5"
+      src={"https://www.youtube.com/embed/" + id}
       title="YouTube video player"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
     ></iframe>
