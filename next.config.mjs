e3f4c2da1b34a54e06a89d86ef82364c1dbb2aa9
teerakan-otk <teerakan.otk@outlook.com>
2026-01-12 20:09:12 +0700

    feat: enable default copy code display in Nextra configuration

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ import nextra from "nextra";
 // Set up Nextra with its configuration
 const withNextra = nextra({
   // ... Add Nextra-specific options here
+  defaultShowCopyCode: true,
   search: { codeblocks: false },
 });
 
